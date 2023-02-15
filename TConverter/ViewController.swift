9f960e8c9760078d10ce8e23567c7ca916cd2ed3
Ley-Towelie <ubrtwelie>
2023-02-15 20:09:41 +0300

    Initial commit

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var upLabel: UILabel!
+    
+    @IBOutlet weak var downLabel: UILabel!
+    ч   
+    
+    @IBOutlet weak var slider: UISlider! {
+        //default of slide and colors
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = -100
+            slider.value = 0
+            //colors
+            slider.thumbTintColor = .green
+            slider.minimumTrackTintColor = .red
+            slider.maximumTrackTintColor = .blue
+        }
     }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        //upLabel
+        let temCels = Int(round(sender.value))
+        upLabel.text = "\(temCels)C"
+        //downLabel
+        let temFah = Int(round(sender.value * 9 / 5) + 32)
+        downLabel.text = "\(temFah)F"
+        
 
+    }
 
 }
 
